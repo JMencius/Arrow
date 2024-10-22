8f5189b8fc4dde4849d509d7b68e6d6764dd20cc
Checunmily <100115060+Checunmily@users.noreply.github.com>
2024-10-22 13:19:09 +0800

    Update run_sra_ont_longbow.sh
    fix path

--- longbow_testing/scripts/run_sra_ont_longbow.sh
+++ longbow_testing/scripts/run_sra_ont_longbow.sh
@@ -6,5 +6,5 @@ base=$(dirname "$script_dir")
 for i in ../data/sra_human_10Kreads/*.fastq; do
 	echo "$i";
 	name=$(basename "$i")
-	python ../../../longbow_code/longbow2.2.0/longbow.py -t 48 -i "$i" -b -o "$base"/results/sra_human/"$i".json;
+	python ../../longbow_code/longbow2.2.0/longbow.py -t 48 -i "$i" -b -o "$base"/results/sra_human/"$i".json;
 done
