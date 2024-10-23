9317d5007b623efd329f49b5f7157e48f0f04027
Jun Mencius <63288733+JMencius@users.noreply.github.com>
2024-10-23 12:12:44 +0800

    Update download_ref.sh

--- COGUK/scripts/download_scripts/download_ref.sh
+++ COGUK/scripts/download_scripts/download_ref.sh
@@ -3,7 +3,7 @@
 mkdir -p ../../ref;
 # git clone https://github.com/artic-network/primer-schemes.git ../../ref;
 
-wget -P ../../ref https://github.com/artic-network/primer-schemes/archive/refs/heads/master.zip;
+wget -nc -P ../../ref https://github.com/artic-network/primer-schemes/archive/refs/heads/master.zip;
 
 unzip ../../ref/master.zip -d ../../ref;
 mv ../../ref/primer-schemes-master/* ../../ref/;
