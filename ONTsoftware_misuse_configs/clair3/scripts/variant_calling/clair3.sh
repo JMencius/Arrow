1e48e1d892464f9624b5a8a4ced499ea1093a79a
Checunmily <100115060+Checunmily@users.noreply.github.com>
2024-11-06 17:40:10 +0800

    Update clair3.sh

--- ONTsoftware_misuse_configs/clair3/scripts/variant_calling/clair3.sh
+++ ONTsoftware_misuse_configs/clair3/scripts/variant_calling/clair3.sh
@@ -7,7 +7,7 @@ conda activate singularity-env;
 ref=../../ref/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.gz
 
 for i in $(ls ../../results/alignment);do
-    echo "$i";
+    echo "${i}";
     for j in r941_prom_hac_g238 r941_prom_hac_g360+g422 r941_prom_sup_g5014 r1041_e82_400bps_hac_v410 r1041_e82_400bps_sup_v410;do
         echo "${j}";
         outdir=../../results/variant_calling/${i}_${j}
