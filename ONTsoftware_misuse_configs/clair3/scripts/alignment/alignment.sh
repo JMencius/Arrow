15e1e9ed2000e82b7eb7586539968427bd683d8c
Jun Mencius <63288733+JMencius@users.noreply.github.com>
2024-11-06 16:17:12 +0800

    Update alignment.sh

--- ONTsoftware_misuse_configs/clair3/scripts/alignment/alignment.sh
+++ ONTsoftware_misuse_configs/clair3/scripts/alignment/alignment.sh
@@ -4,9 +4,6 @@ conda_base=$(conda info --base);
 source "$conda_base"/etc/profile.d/conda.sh;
 conda activate minimap2;
 
-# Download the hg38 reference genome
-mkdir -p ../../ref;
-wget -P ../../ref/ https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/001/405/GCA_000001405.15_GRCh38/seqs_for_alignment_pipelines.ucsc_ids/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.gz
 
 refseq_hg38=../../ref/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.gz
 
