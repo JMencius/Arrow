6d6b5a0221f85d8620e13cc0e89148c5d89b1261
Jun Mencius <63288733+JMencius@users.noreply.github.com>
2024-11-07 18:16:11 +0800

    Update run_all.sh

--- longbow_training/scripts/run_all.sh
+++ longbow_training/scripts/run_all.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-# Pipeline
+# build directory structure
+mkdir -p ../results;
+
+
 # Conduct leave-one-out test to determine the best lag for R9G4, R9G6, R9D0, R10G6, and R10D0
 
 conda_base=$(conda info --base);
 source "$conda_base"/etc/profile.d/conda.sh;
 conda activate ont-longbow;
 
-python autocorrelation_LOO_lag.py -i ../results/model -o ../results/R9G4_autocorrelation_lag_accuracy.txt --subject R9G4;
-python autocorrelation_LOO_lag.py -i ../results/model -o ../results/R9G6_autocorrelation_lag_accuracy.txt --subject R9G6;
-python autocorrelation_LOO_lag.py -i ../results/model -o ../results/R9D0_autocorrelation_lag_accuracy.txt --subject R9D0;
-python autocorrelation_LOO_lag.py -i ../results/model -o ../results/R10G6_autocorrelation_lag_accuracy.txt --subject R10G6;
-python autocorrelation_LOO_lag.py -i ../results/model -o ../results/R10D0_autocorrelation_lag_accuracy.txt --subject R10D0;
+python autocorrelation_LOO_lag.py -i ../data/model -o ../results/R9G4_autocorrelation_lag_accuracy.txt --subject R9G4;
+python autocorrelation_LOO_lag.py -i ../data/model -o ../results/R9G6_autocorrelation_lag_accuracy.txt --subject R9G6;
+python autocorrelation_LOO_lag.py -i ../data/model -o ../results/R9D0_autocorrelation_lag_accuracy.txt --subject R9D0;
+python autocorrelation_LOO_lag.py -i ../data/model -o ../results/R10G6_autocorrelation_lag_accuracy.txt --subject R10G6;
+python autocorrelation_LOO_lag.py -i ../data/model -o ../results/R10D0_autocorrelation_lag_accuracy.txt --subject R10D0;
