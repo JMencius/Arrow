38bf5dfce440ce9b7da9b524d41f463f2307017c
Jun Mencius <63288733+JMencius@users.noreply.github.com>
2024-11-07 16:45:56 +0800

    Update download_ngs.sh

--- ONTsoftware_misuse_configs/yak/scripts/download_ngs.sh
+++ ONTsoftware_misuse_configs/yak/scripts/download_ngs.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# build directory structure
+mkdir -p ../data;
+
+
 # retrive the ~30X PCR-free Illumina 150bp sequencing file from human pangenome
 wget -P ../data https://s3-us-west-2.amazonaws.com/human-pangenomics/NHGRI_UCSC_panel/HG002/hpp_HG002_NA24385_son_v1/ILMN/downsampled/HG002_HiSeq30x_subsampled_R1.fastq.gz;
 wget -P ../data https://s3-us-west-2.amazonaws.com/human-pangenomics/NHGRI_UCSC_panel/HG002/hpp_HG002_NA24385_son_v1/ILMN/downsampled/HG002_HiSeq30x_subsampled_R2.fastq.gz;
