be15e668d364f913685f73fcc93d1eb583a89d55
Jun Mencius <63288733+JMencius@users.noreply.github.com>
2024-11-07 17:55:40 +0800

    Update run_medaka_polishing.sh

--- ONTsoftware_misuse_configs/medaka/scripts/run_medaka_polishing.sh
+++ ONTsoftware_misuse_configs/medaka/scripts/run_medaka_polishing.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# build directory structure
+for i in R9G4FAST R9G4HAC R9G6FAST R9G6HAC R9G6SUP R10D0HAC R10D0SUP;do
+	for j in R9G4FAST R9G4HAC R9G6FAST R9G6HAC R9G6SUP R10D0HAC R10D0SUP;do
+		mkdir -p ../results/"$i"/"$i"_"$j";
+	done
+done
+
+
 # medaka polishing for R9G4FAST
 bash R9G4FAST_medaka.sh;
 
