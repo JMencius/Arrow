f2769d14000579853738b349264fc3bf932d6a85
Jun Mencius <63288733+JMencius@users.noreply.github.com>
2024-11-07 13:47:06 +0800

    Update run_all.sh

--- Others/guppy_similarity/scripts/run_all.sh
+++ Others/guppy_similarity/scripts/run_all.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Pipeline
-# build directory structure
-mkdir -p ../results/seq_identity;
-mkdir -p ../results/QV_similarity;
-
 # 1. Evaluate the sequence identity between HG002 R9 Guppy versions
 bash HG002_R9_seq_identity.sh;
 
