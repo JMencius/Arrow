8c8d8b9930b1fec58c69a0c13caff3905dd9a265
Jun Mencius <63288733+JMencius@users.noreply.github.com>
2024-11-07 17:55:24 +0800

    Update run_all.sh

--- ONTsoftware_misuse_configs/medaka/scripts/run_all.sh
+++ ONTsoftware_misuse_configs/medaka/scripts/run_all.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-# build directory structure
-for i in R9G4FAST R9G4HAC R9G6FAST R9G6HAC R9G6SUP R10D0HAC R10D0SUP;do
-	for j in R9G4FAST R9G4HAC R9G6FAST R9G6HAC R9G6SUP R10D0HAC R10D0SUP;do
-		mkdir -p ../results/"$i"/"$i"_"$j";
-	done
-done
-
-
-
 # Pipeline
 # 1. Run `Medaka` polishing for R9G4FAST, R9G4HAC, R9G6FAST, R9G6HAC, R9G6SUP, R10D0HAC, and R10D0SUP data
 bash run_medaka_polishing.sh;
