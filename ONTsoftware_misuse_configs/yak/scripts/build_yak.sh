9e41a72622ea7d2d2e9a5e468a4e3fe57e559d81
Jun Mencius <63288733+JMencius@users.noreply.github.com>
2024-11-07 16:47:09 +0800

    Update build_yak.sh

--- ONTsoftware_misuse_configs/yak/scripts/build_yak.sh
+++ ONTsoftware_misuse_configs/yak/scripts/build_yak.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+# build directory structure
+mkdir -p ../results;
+
+# activate yak env
 conda_base=$(conda info --base);
 source "$conda_base"/etc/profile.d/conda.sh;
 conda activate yak;
 
-
-# build yak reference using NGS sequencing data
+# build yak reference hash table using NGS sequencing data
 yak count -b 37 -t 32 -o ../results/sr.yak <(cat ../data/HG002_HiSeq30x_subsampled_R1.fastq) <(cat ../data/HG002_HiSeq30x_subsampled_R2.fastq);
